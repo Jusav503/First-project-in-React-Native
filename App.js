aedf729ebafcb503e2286f5edc92c3ba17029068
Justice Velasco <justicevgomez001@gmail.com>
2021-09-05 16:45:36 +0200

    version alfa

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  StatusBar,
+} from "react-native";
 import CoinItem from "./components/CoinItem";
 
 export default function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  const [refreshing, setRefreshing] = useState(false);
 
   const loadData = async () => {
     const res = await fetch(
@@ -18,15 +27,57 @@ export default function App() {
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#000" />
+
+      <View style={styles.header}>
+        <Text style={styles.title}>JusavMarket</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search"
+          placeholderTextColor="#858585"
+          onChangeText={(text) => setSearch(text)}
+          autoCapitalize="none"
+          autoCompleteType="off"
+        />
+      </View>
+
       <FlatList
-        data={coins}
+        style={styles.list}
+        data={coins.filter(
+          (coin) =>
+            coin.name.toLowerCase().includes(search) ||
+            coin.symbol.toLowerCase().includes(search)
+        )}
+        refreshing={refreshing}
+        onRefresh={async () => {
+          setRefreshing(true);
+          await loadData();
+          setRefreshing(false);
+        }}
         renderItem={({ item }) => {
-          return <CoinItem coin={item}/>
+          return <CoinItem coin={item} />;
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { backgroundColor: "#000", alignItems: "center", flex: 1 },
+  list: { width: "90%" },
+  title: { color: "#fff", fontSize: 20 },
+  header: {
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#696969",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
+  },
+});
